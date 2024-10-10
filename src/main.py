ef373820b0de47b30da8d8ec4c9cb6cad15fc97a
ssungcoding <tjdgus2532@gmail.com>
2024-10-10 15:59:29 +0900

    횟수추가

--- src/main.py
+++ src/main.py
@@ -25,6 +25,8 @@ async def analyze_video(video: UploadFile = File(...)):
     # 비디오 파일을 Mediapipe로 분석
     cap = cv2.VideoCapture(file_location)
     mp_pose = mp.solutions.pose
+    motion_count = 0  # 모션 카운트를 위한 변수 초기화
+
     with mp_pose.Pose(static_image_mode=False, min_detection_confidence=0.5) as pose:
         while cap.isOpened():
             ret, frame = cap.read()
@@ -32,6 +34,9 @@ async def analyze_video(video: UploadFile = File(...)):
                 break
             results = pose.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
             if results.pose_landmarks and is_badminton_motion(results.pose_landmarks):
-                return {"pose_detected": True}  # 배드민턴 동작 감지됨
+                motion_count += 1  # 모션이 감지되면 카운트 증가
+                if motion_count >= 200:  # 카운트가 200 이상이면 True 반환
+                    cap.release()
+                    return {"pose_detected": True}  # 배드민턴 동작 감지됨
     cap.release()
     return {"pose_detected": False}  # 배드민턴 동작 감지되지 않음
